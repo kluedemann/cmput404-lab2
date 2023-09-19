655bee027c05bbc5134bd2e43a3a52e02ef53e4e
Kai Luedemann <kluedema@ualberta.ca>
2023-09-18 18:40:02 -0600

    7. Forward the request through the proxy server and return the response

--- proxy_client.py
+++ proxy_client.py
@@ -1,11 +1,21 @@
 import socket
 
 
+BUFFER_SIZE = 4096
+
+
 def main():
     proxy_address = "localhost", 8080
+    host = "www.google.com"
+    request = f'GET / HTTP/1.0\r\nHost: {host}\r\n\r\n'
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
         sc.connect(proxy_address)
+        sc.sendall(request.encode())
         sc.shutdown(socket.SHUT_WR)
+        data = sc.recv(BUFFER_SIZE)
+        while data:
+            print(data)
+            data = sc.recv(BUFFER_SIZE)
 
 
 if __name__ == "__main__":
--- proxy_server.py
+++ proxy_server.py
@@ -1,11 +1,45 @@
 import socket
 
 
-def main():
+BUFFER_SIZE = 4096
+
+
+def create_request(data):
     address = "www.google.com", 80
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
         sc.connect(address)
+        sc.sendall(data)
         sc.shutdown(socket.SHUT_WR)
+        response = b''
+        data = sc.recv(BUFFER_SIZE)
+        while data:
+            response += data
+            data = sc.recv(BUFFER_SIZE)
+    return response
+
+
+def start_server():
+    address = "localhost", 8080
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        # Bind and listen
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(address)
+        s.listen(2)
+        
+        # continuously listen for connections
+        while True:
+            conn, addr = s.accept()
+            print("Connected by", addr)
+            
+            #recieve data, wait a bit, then send it back
+            full_data = conn.recv(BUFFER_SIZE)
+            response = create_request(full_data)
+            conn.sendall(response)
+            conn.close()
+
+def main():
+    start_server()
 
 
 if __name__ == "__main__":
