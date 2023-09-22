9b959b8d00216abc6287040ae4a56e6aa068124d
Kai Luedemann <kluedema@ualberta.ca>
2023-09-21 21:35:16 -0600

    Added server classes

--- echo_server.py
+++ echo_server.py
@@ -3,39 +3,62 @@ import socket
 import time
 from threading import Thread
 
+
+class Server:
+
+    def __init__(self, handler) -> None:
+        self.handler = handler
+
+    def accept_connections(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    
+            #QUESTION 3
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            
+            #bind socket to address
+            s.bind((HOST, PORT))
+            #set to listening mode
+            s.listen(2)
+            
+            #continuously listen for connections
+            while True:
+                conn, addr = s.accept()
+                print("Connected by", addr)
+                
+                self.handler.handle(conn)
+
+
+class EchoHandler:
+
+    def handle(self, conn):
+        full_data = conn.recv(BUFFER_SIZE)
+        time.sleep(0.5)
+        print(full_data)
+        conn.sendall(full_data)
+        conn.close()
+
+
+class ThreadedHandler:
+
+    def __init__(self, handler):
+        self.handler = handler
+
+    def handle(self, conn):
+        #recieve data, wait a bit, then send it back
+        t = Thread(target=self.handler.handle_connection, args=(conn,))
+        t.start()
+
+
 #define address & buffer size
 HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
 
 
-def handle_connection(conn):
-    full_data = conn.recv(BUFFER_SIZE)
-    time.sleep(0.5)
-    print(full_data)
-    conn.sendall(full_data)
-    conn.close()
-
-
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-        #QUESTION 3
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        #bind socket to address
-        s.bind((HOST, PORT))
-        #set to listening mode
-        s.listen(2)
-        
-        #continuously listen for connections
-        while True:
-            conn, addr = s.accept()
-            print("Connected by", addr)
-            
-            #recieve data, wait a bit, then send it back
-            t = Thread(target=handle_connection, args=(conn,))
-            t.start()
+    server = Server(EchoHandler())
+    server = Server(ThreadedHandler(EchoHandler))
+    server.accept_connections()
 
 if __name__ == "__main__":
     main()
--- proxy_server.py
+++ proxy_server.py
@@ -1,68 +1,37 @@
 import socket
 from threading import Thread
-
+from echo_server import Server, ThreadedHandler
 
 BUFFER_SIZE = 4096
 
+class ProxyHandler:
 
-def create_request(data):
-    address = "www.google.com", 80
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
-        sc.connect(address)
-        sc.sendall(data)
-        sc.shutdown(socket.SHUT_WR)
-        response = b''
-        data = sc.recv(BUFFER_SIZE)
-        while data:
-            response += data
+    def handle(self, conn):
+        #recieve data, wait a bit, then send it back
+        full_data = conn.recv(BUFFER_SIZE)
+        response = self.create_request(full_data)
+        conn.sendall(response)
+        conn.close()
+
+    def create_request(self, data):
+        address = "www.google.com", 80
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc:
+            sc.connect(address)
+            sc.sendall(data)
+            sc.shutdown(socket.SHUT_WR)
+            response = b''
             data = sc.recv(BUFFER_SIZE)
-    return response
+            while data:
+                response += data
+                data = sc.recv(BUFFER_SIZE)
+        return response
 
 
-def start_threaded_server():
-    address = "localhost", 8080
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
-        # Bind and listen
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(address)
-        s.listen(2)
-        
-        # continuously listen for connections
-        while True:
-            conn, addr = s.accept()
-            print("Connected by", addr)
-            
-            t = Thread(target=handle_connection, args=(conn,))
-            t.start()
-
-
-def handle_connection(conn):
-    
-    #recieve data, wait a bit, then send it back
-    full_data = conn.recv(BUFFER_SIZE)
-    response = create_request(full_data)
-    conn.sendall(response)
-    conn.close()
-
-
-def start_server():
-    address = "localhost", 8080
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
-        # Bind and listen
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(address)
-        s.listen(2)
-        
-        # continuously listen for connections
-        while True:
-            conn, addr = s.accept()
-            print("Connected by", addr)
-            handle_connection(conn)
 
 def main():
-    start_threaded_server()
+    server = Server(ProxyHandler())
+    server = Server(ThreadedHandler(ProxyHandler()))
+    server.accept_connections()
 
 
 if __name__ == "__main__":
